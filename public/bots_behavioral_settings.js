99a669a04d274b9f36494d2bf3183261a166f737
danger_electrodes <adrien.pelletier.la.roche@gmail.com>
2026-01-24 01:48:54 +0100

    initial commit

--- public/bots_behavioral_settings.js
+++ public/bots_behavioral_settings.js
@@ -106,7 +106,7 @@
       overlayDiv.style.top = '25px';
       overlayDiv.style.left = 'right';
       overlayDiv.style.width = '100%';
-      overlayDiv.style.height = '100%';
+      overlayDiv.style.height = 'calc(100%-50px)';
       overlayDiv.style.zIndex = '9999';
       overlayDiv.style.pointerEvents = 'none';
       overlayDiv.style.border = '2px solid red';
